0dadb518483e301492488a864b05865d135670c4
indianajone <mmer555@hotmail.com>
2016-01-28 01:59:09 +0700

    temporary use 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,47 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 Not found</title>
+@extends('layouts.master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Opps! Are you lost?</div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+    {{-- <div class="notfound">
+        <h3 class="notfound__body">{{ trans('common.notfound') }}</h3>
+    </div> --}}
+@stop
