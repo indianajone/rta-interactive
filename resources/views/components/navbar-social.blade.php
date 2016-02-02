4276a217cf4ca16b20f523b2d94df15decfc0c05
indianajone <mmer555@hotmail.com>
2016-02-03 00:21:32 +0700

    add social links

--- resources/views/components/navbar-social.blade.php
+++ resources/views/components/navbar-social.blade.php
@@ -1,6 +1,6 @@
 <ul class="navbar-social">
     <li>
-        <a class="navbar-social__button" href="#">
+        <a class="navbar-social__button" target="_blank" href="//www.facebook.com/armytour.go.th" >
             <span class="fa-stack fa-lg">
                 <i class="fa fa-circle fa-stack-2x"></i>
                 <i class="fa fa-facebook fa-stack-1x"></i>
@@ -8,7 +8,7 @@
         </a>
     </li>
     <li>
-        <a class="navbar-social__button" href="#">
+        <a class="navbar-social__button" target="_blank" href="//plus.google.com/communities/106885863597892764886">
             <span class="fa-stack fa-lg">
                 <i class="fa fa-circle fa-stack-2x"></i>
                 <i class="fa fa-google-plus fa-stack-1x"></i>
