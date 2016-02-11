96e9164ef2087b79829cd8251eb4e361b69be0bd
indianajone <mmer555@hotmail.com>
2016-02-12 01:30:52 +0700

    add styling for phone and mail link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="format-detection" content="telephone=no">
     <meta name="csrf_token" id="csrf_token" value={{ csrf_token() }}>
     <meta name="author" content="สำนักงานส่งเสริมการท่องเที่ยวกองทัพบก">
     @yield ('meta')
