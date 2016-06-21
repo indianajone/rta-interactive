6036d47f368cda119fd1ac9418fd01cf579f3d5a
indianajone <mmer555@hotmail.com>
2016-06-21 17:21:53 +0700

    clean up

--- resources/views/places/partials/photo.blade.php
+++ resources/views/places/partials/photo.blade.php
@@ -12,23 +12,5 @@
             >
             </gallery>
         </readmore>
-        <!-- <readmore 
-            max-height="300" 
-            text="{{ collect(trans('common.buttons.viewall')) }}"
-            show="{{ $place->photos->count() > 3 }}"
-        >
-             @foreach($place->photos->chunk(3) as $set)
-                <div class="row">
-                    @foreach($set as $photo)     
-                        <div class="place__image">
-                            <a href="{{ asset($photo->path) }}" v-photoswipe>
-                                <img src="{{ asset($photo->thumbnail_path) }}" alt="{{ $photo->title }}">
-                                <h4 class="text-center">{{ $photo->title }}</h4>
-                            </a>
-                        </div>
-                    @endforeach
-                </div>
-            @endforeach
-        </readmore> -->
     </div>
 @endif
