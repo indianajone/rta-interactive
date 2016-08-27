6580625fb7fe5e98aac6f98fe5cf596d7f2f266f
indianajone <mmer555@hotmail.com>
2016-08-27 16:24:10 +0700

    move modal to it own partial and use it with maker

--- resources/views/places/partials/marker.blade.php
+++ resources/views/places/partials/marker.blade.php
@@ -6,18 +6,11 @@
             text="{{ collect(trans('common.buttons.viewall')) }}"
             show="{{ $place->markers->count() > 3 }}"
         >
-             @foreach($place->markers->chunk(3) as $set)
-                <div class="row">
-                    @foreach($set as $marker)
-                        <div class="place__image">
-                             <a href="{{ asset($marker->path) }}" data-lity>
-                                <img src="{{ asset($marker->thumbnail_path) }}" alt="{{ $marker->title }}">
-                                <h4 class="text-center">{{ $marker->title }}</h4>
-                            </a>
-                        </div>
-                    @endforeach
-                </div>
-            @endforeach
+            <gallery 
+                photos="{{ $place->markers }}"
+                buttons="{{ collect(trans('common.buttons.viewall')) }}"
+            >
+            </gallery>
         </readmore>
     </div>
 @endif
