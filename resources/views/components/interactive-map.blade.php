601709e791a80b36a79174398ad49af5e1151d21
indianajone <mmer555@hotmail.com>
2016-10-13 23:43:35 +0700

    extract map and form control

---
+++ resources/views/components/interactive-map.blade.php
@@ -0,0 +1,55 @@
+<form 
+    @submit.prevent="navigateMe"
+    @keyup.prevent.enter="navigateMe"
+    accept-charset="utf-8"
+    v-cloak
+>
+    <fieldset class="top">
+        <mode :mode.sync="route.travelMode"></mode>
+        
+        <origin :origin.sync="route.origin" inline-template>
+            <div class="form-group">
+                <input 
+                    v-el:origin
+                    v-model="value"
+                    @blur="onBlur"
+                    @focus="onFocus"
+                    type="text"
+                    class="form-control"
+                    placeholder="{{ trans('map.origin') }}"
+                    required >
+            </div>
+        </origin>
+        
+        <waypoint :waypoints.sync="route.waypoints"></waypoint>
+        
+        @include('components.destination')
+    </fieldset>
+    @if ($nearby)
+    <fieldset class="bottom" v-if="route.travelMode == 'DRIVING'">
+        <div class="waypoints">
+            <legend>{{ trans('map.nearby.title') }}</legend>
+            <div class="col-xs-12">
+                <input v-model="nearby" type="checkbox">
+                {{ trans('map.nearby.show') }}
+            </div>
+        </div>
+    </fieldset>
+    @endif
+    <fieldset class="bottom" v-if="route.travelMode == 'DRIVING'">
+        <div class="waypoints">
+            <legend>{{ trans('map.waypoints.title') }}</legend>
+            <div class="col-xs-6" v-for="thing in things">
+                <label class="checkbox-inline">
+                     <input 
+                        v-model="thing.selected"
+                        type="checkbox"
+                    > @{{ thing.name }}    
+                </label>
+            </div>
+        </div>
+    </fieldset>
+    <fieldset class="direction">
+        <google-panel id="direction"></google-panel>
+    </fieldset>
+</form>
--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -1,65 +1,19 @@
 @extends('layouts.master')
 
 @section('content')
+    
+    <div class="map-wrapper">
+        <interactive-map inline-template 
+            :things="{{ $options }}"
+            :place="{{ $place or '{}' }}"
+            :nears="{{ $nearby }}"
+            :nearby="true"
+        >
+            <div class="interactive-map">
+                @include('components.interactive-map')
+                <google-map :route="route" :things="selectedThings"></google-map>
+            </div>
+        </interactive-map>
+    </div>
 
-    <interactive-map inline-template :things="{{ $options }}" :place="{{ $place or '{}' }}" :nears="{{ $nearby }}" :nearby="true">
-        <div id="map" class="interactive-map">
-            <form 
-                @submit.prevent="navigateMe"
-                @keyup.prevent.enter="navigateMe"
-                accept-charset="utf-8"
-                v-cloak
-            >
-                <fieldset class="top">
-                    <mode :mode.sync="route.travelMode"></mode>
-                    
-                    <origin :origin.sync="route.origin" inline-template>
-                        <div class="form-group">
-                            <input 
-                                v-el:origin
-                                v-model="value"
-                                @blur="onBlur"
-                                @focus="onFocus"
-                                type="text"
-                                class="form-control"
-                                placeholder="{{ trans('map.origin') }}"
-                                required >
-                        </div>
-                    </origin>
-                    
-                    <waypoint :waypoints.sync="route.waypoints"></waypoint>
-                    
-                    @include('components.destination')
-                </fieldset>
-                @if ($nearby)
-                <fieldset class="bottom" v-if="route.travelMode == 'DRIVING'">
-                    <div class="waypoints">
-                        <legend>{{ trans('map.nearby.title') }}</legend>
-                        <div class="col-xs-12">
-                            <input v-model="nearby" type="checkbox">
-                            {{ trans('map.nearby.show') }}
-                        </div>
-                    </div>
-                </fieldset>
-                @endif
-                <fieldset class="bottom" v-if="route.travelMode == 'DRIVING'">
-                    <div class="waypoints">
-                        <legend>{{ trans('map.waypoints.title') }}</legend>
-                        <div class="col-xs-6" v-for="thing in things">
-                            <label class="checkbox-inline">
-                                 <input 
-                                    v-model="thing.selected"
-                                    type="checkbox"
-                                > @{{ thing.name }}    
-                            </label>
-                        </div>
-                    </div>
-                </fieldset>
-                <fieldset class="direction">
-                    <google-panel id="direction"></google-panel>
-                </fieldset>
-            </form>
-            <google-map :route="route" :things="selectedThings"></google-map>
-        </div>
-    </interactive-map>
 @stop
